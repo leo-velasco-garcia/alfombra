ff208c52fed75abe362b95b6833c0f5e5fe23586
Leo Velasco García <leo@172-15-25-39.lightspeed.hstntx.sbcglobal.net>
2025-10-20 18:57:55 +0200

    pixelBlast rehecho

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,17 +1,18 @@
-import React, { useLayoutEffect, useRef } from 'react'
-import Button from '../Otros/Button'
-import "./Landing.css"
-import { useContext } from 'react'
-import { AppContext } from '../../AppContext'
-import { gsap } from 'gsap'
+import React, { useLayoutEffect, useRef } from 'react';
+import Button from '../Otros/Button';
+import "./Landing.css";
+import { useContext } from 'react';
+import { AppContext } from '../../AppContext';
+import { gsap } from 'gsap';
+import PixelBlast from '../PixelBlast/PixelBlast';
 
 
 const Landing = () => {
-    const {page, setPage, setAbrir} = useContext(AppContext);
+    const { page, setPage, setAbrir } = useContext(AppContext);
     const rootRef = useRef(null)
 
     useLayoutEffect(() => {
-        if(page !== 'Landing') return;
+        if (page !== 'Landing') return;
         const ctx = gsap.context(() => {
             gsap.from([".headerLanding .titulo2", ".buttoncontrol"], {
                 opacity: 0,
@@ -30,9 +31,31 @@ const Landing = () => {
                 <h2 className="titulo2">Landing</h2>
             </div>
             <div className="buttoncontrol">
-                <Button content="Quiero crear!" onClick={() => setAbrir(true)}/>
+                <Button content="Quiero crear!" onClick={() => setAbrir(true)} />
             </div>
-            </section>
+
+            <div style={{ width: '100%', height: '600px', position: 'relative'}}>
+                <PixelBlast
+                    variant="circle"
+                    pixelSize={6}
+                    color="#B19EEF"
+                    patternScale={3}
+                    patternDensity={1.2}
+                    pixelSizeJitter={0.5}
+                    enableRipples
+                    rippleSpeed={0.4}
+                    rippleThickness={0.12}
+                    rippleIntensityScale={1.5}
+                    liquid
+                    liquidStrength={0.12}
+                    liquidRadius={1.2}
+                    liquidWobbleSpeed={5}
+                    speed={0.6}
+                    edgeFade={0.25}
+                    transparent
+                />
+            </div>
+        </section>
     )
 }
 
